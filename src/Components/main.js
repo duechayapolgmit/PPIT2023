d5402ea6da4d3e248974607dc375a8f736f90b78
duechayapolgmit <duechayapolsbs@gmail.com>
2023-02-15 11:18:39 +0000

    Initial set up for retrieving the coordinates of a user

--- src/Components/main.js
+++ src/Components/main.js
@@ -2,20 +2,33 @@ import React from "react";
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 export class Main extends React.Component {
+
+    state = {
+        latitude: 0,
+        longitude: 0
+    }
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(pos => {
+            this.setState({latitude:pos.coords.latitude, longitude:pos.coords.longitude})
+        })
+    }
+
     render() {
         const mapStyles = {
             width: '100%',
             height: '100%',
         };
+        console.log(this.state.latitude + ", "+this.state.longitude);
         return (
             <Map
                 google={this.props.google}
                 zoom={8}
                 style={mapStyles}
-                initialCenter={{ lat: 53.270962, lng: -9.062691 }}
+                initialCenter={{ lat: this.state.latitude, lng: this.state.longitude }}
 
             >
-                <Marker position={{ lat: 53.27078784297829, lng: -9.05478100521472 }} />
+                <Marker position={{ lat: this.state.latitude, lng: this.state.longitude }} />
             </Map>
 
         )
