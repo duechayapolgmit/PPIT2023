66536272d8d59f4ce0f0896760446c9ceadbe5e5
AdrianHolkLebinski <G00395620@gmit.ie>
2023-02-22 22:50:55 +0000

    Feature #4 finished, markers are displaying on map based on API information

--- src/Components/main.js
+++ src/Components/main.js
@@ -23,25 +23,18 @@ export class Main extends React.Component {
     componentDidMount() {
         var markersLocation, currentPosition;
         navigator.geolocation.getCurrentPosition(pos => {
-            this.setState({ currentPos: {latitude: pos.coords.latitude, longitude: pos.coords.longitude},
-                markers: []
-             })
-            //currentPosition = { latitude: pos.coords.latitude, longitude: pos.coords.longitude }
+            this.setState({ latitude: pos.coords.latitude, longitude: pos.coords.longitude})
         })
 
         fetch('https://services-eu1.arcgis.com/Zmea819kt4Uu8kML/arcgis/rest/services/CarParkingOpenData/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson')
             .then((response) => response.json())
             .then((responseJson) => {
+                console.log(responseJson);
                 this.setState({ markers: responseJson.features })
-                //markersLocation = responseJson.features
             })
             .catch((error) => {
                 console.error(error);
             });
-
-        this.setState({ currentPos: currentPosition, markers: markersLocation })
-        console.log(this.state)
-        console.log(markersLocation)
     }
 
     onMarkerClick = (props, marker, e) => {
@@ -72,7 +65,7 @@ export class Main extends React.Component {
                     <InfoCard space={{empty:200, full:200}} type={"Pay/Display"} marker={this.state.activeMarker.name}/>
                 </InfoWindow>
 
-                <Markers markers={this.state.markers} Reload={this.componentDidMount}></Markers>
+                <Markers markers={this.state.markers}></Markers>
             </Map>
 
         )
--- src/Components/markers.js
+++ src/Components/markers.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { MarkerItem } from "./markerItem";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-export class Markers extends React.Component{
-    render(){
+export class Markers extends React.Component {
+    render() {
+        const { google, map } = this.props;
+
+        if (!google || !map) {
+            return null;
+        }
         return this.props.markers.map(
-            (marker)=>{
-                return <MarkerItem marker={marker} Reload={this.componentDidMount}></MarkerItem>
+            (marker) => {
+                return <Marker
+                    position={{ lat: marker.geometry.coordinates[1], lng: marker.geometry.coordinates[0] }}
+                    map={map}
+                    google={google}
+
+                ></Marker>
             }
         );
     }
