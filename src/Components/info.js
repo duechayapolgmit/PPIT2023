20ce522f194396a54f108eec0384b8ff7f2ec261
FrostItsShadow <98172456+FrostItsShadow@users.noreply.github.com>
2023-03-09 01:47:36 +0000

    Added directions function that lets the user get the directions towards a chosen parking space

--- src/Components/info.js
+++ src/Components/info.js
@@ -1,24 +1,25 @@
 import React from "react";
+import directionsImage from '../Images/directions-dark.png';
 
-export class InfoCard extends React.Component{
+export class InfoCard extends React.Component {
 
     // Get percent HTML. Will not show if capacity is 0.
-    getPercent(name, occupy, full){
+    getPercent(name, occupy, full) {
         if (name === "Current Location" || full === 0) return;
         let percentage = this.getPercentage(occupy, full).toFixed(0);
         let bgColour = this.getPercentageColour(percentage);
-        return <span className={`${bgColour} pl-4 pr-4`}>{percentage+"%"}</span>
+        return <span className={`${bgColour} pl-4 pr-4`}>{percentage + "%"}</span>
     }
 
     // Get percentage from values parsed in (uses space.empty and space.full values)
-    getPercentage(empty, full){
+    getPercentage(empty, full) {
         if (this.props.space.full === 0) return 0;
         let percent = parseFloat(empty) / parseFloat(full);
-        return percent*100;
+        return percent * 100;
     }
 
     // Get the colour for the percentage background
-    getPercentageColour(percent){
+    getPercentageColour(percent) {
         // may revisit later
         if (percent < 25) return "bg-green-500";
         if (percent < 50) return "bg-yellow-300";
@@ -27,19 +28,35 @@ export class InfoCard extends React.Component{
     }
 
     // Get capacity HTML. Will not show if capacity is 0.
-    getCapacity(occupy, capacity){
+    getCapacity(occupy, capacity) {
         if (capacity === 0) return;
         else return <p>Capacity: {occupy} / {capacity}</p>;
     }
 
-    render(){
+
+
+    render() {
         let percent = this.getPercent(this.props.name, this.props.space.occupied, this.props.space.full);
         let capacity = this.getCapacity(this.props.space.occupied, this.props.space.full);
+
+        var origin = this.props.lat + "," + this.props.lon;
+        let destination = 0;
+        if (this.props.marker.position != undefined) destination = this.props.marker.position;
+
+        //generate URL for directions
+        var longitude = destination.lng;
+        var latitude = destination.lat;
+        var url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${latitude},${longitude}`;
+
+        let imageStyle = { width: '40px', height: '40px' };
+        if(this.props.marker.name == "Current Location")imageStyle = { width: '0px', height: '0px' };
         return (
             <div className="container text-left">
-                <h1 className="text-xl font-bold">{percent} {this.props.marker}</h1>
+                <h1 className="text-xl font-bold">{percent} {this.props.marker.name}</h1>
                 {capacity}
                 <p>{this.props.type}</p>
+                <a href={url}><img src={directionsImage} style={imageStyle}/></a>
+                
             </div>
         )
     }
--- src/Components/listMarkers.js
+++ src/Components/listMarkers.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react"
 import markerImage from '../Images/location_icon.png';
+import directionsImage from '../Images/directions.png';
 
 export default function MarkersSidebar(props){
 
     const [markers, setMarkers] = useState(props.markers);
 
+
     useEffect( () => {
         setMarkers(props.markers);
     },[props.markers])
-
     return (
         <div id="markers-list-menu" className="overlay list-markers bg-teal-500">
             <h1 className="text-lg">List of Markers</h1><hr/>
-            <MarkersList markers={markers}/>
+            <MarkersList markers={markers} lat={props.lat} lon={props.lon}/>
         </div>
     )
 }
@@ -20,7 +21,7 @@ export default function MarkersSidebar(props){
 class MarkersList extends React.Component {
     render(){
         return this.props.markers.map( (marker) => {
-            return <MarkerInfo marker={marker}/>
+            return <MarkerInfo marker={marker} lat={this.props.lat} lon={this.props.lon}/>
         })
     }
 }
@@ -34,11 +35,24 @@ class MarkerInfo extends React.Component {
         // handles capacity formatting
         let capacity = 0;
         if (this.props.marker.properties.NO_SPACES != "") capacity = this.props.marker.properties.NO_SPACES;
+        
+        var origin = this.props.lat + "," + this.props.lon;
+
+        //handles destination formatting
+        let destination = 0;
+        if (this.props.marker.geometry.coordinates[0] != "") destination = this.props.marker.geometry.coordinates;
+
+        //generate URL for directions
+        var longitude = destination[0];
+        var latitude = destination[1];
+        var url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${latitude},${longitude}`;
+
         return (
             <div>
                 <div className="list-markers-single">
                     <div className="list-markers-image"><img src={markerImage}/>{distance}km</div>
                     <p>{this.props.marker.properties.NAME}<br/>Capacity: 0 / {capacity}<br/>{this.props.marker.properties.TYPE}</p>
+                    <a href={url}><img src={directionsImage}/></a>
                 </div>
                 <hr/>
             </div>
